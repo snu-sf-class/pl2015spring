e59aee9eb6771b953b01534faea885dccc445681
U-gil-pc\gil <gil.hur@sf.snu.ac.kr>
2015-03-21 15:00:30 +0900

    Fixed a typo.

--- sf/Assignment_02.v
+++ sf/Assignment_02.v
@@ -125,7 +125,7 @@ Proof.
 
 
 
-(** **** Problem #7 : 4 stars (mult_comm) *)
+(** **** Problem #7 : 4 stars (plus_swap) *)
 (** Use [assert] to help prove this theorem if necessary.  
     You shouldn't need to use induction. *)
 
